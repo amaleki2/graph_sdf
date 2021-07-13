a3dbaf3171536fb9dce217251571de0ce1f31d74
Amir Maleki <amir.maleki.zamenjani@gmail.com>
2021-07-13 06:50:26 -0600

    save predictions test time

--- src/test.py
+++ src/test.py
@@ -9,6 +9,7 @@ def test_and_visualize(model,
                        save_losses=True,
                        save_3d_surface=True,
                        save_2d_contours=True,
+                       save_predictions=False,
                        save_folder_name="save"):
 
     if save_losses:
@@ -38,7 +39,9 @@ def test_and_visualize(model,
                                  os.path.join(os.getcwd(), save_folder_name, 'pred_surface_mesh%d.obj' % i)]
                 plot_surface_mesh(true_val, pred_val, save_names=save_name_msh, level=0)
 
-
             predictions.append([points, true_val, pred_val])
 
-    return predictions
+        if save_predictions:
+            save_name_preds = os.path.join(os.getcwd(), save_folder_name, 'preds.npy')
+            np.save(save_name_preds, predictions)
+
